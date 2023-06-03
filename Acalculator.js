f0fd5a553ad70fda2ca606e309504ead523bebb8
ChaimaeChr <123027484+CXaymae@users.noreply.github.com>
2023-06-03 23:34:59 +0200

    fix calculation precision

--- Acalculator.js
+++ Acalculator.js
@@ -8,12 +8,12 @@ function calculate() {
         let ageInMin = ageInS / 60;
         let ageInHrs = ageInMin / 60;
         let ageInDays = ageInHrs / 24;
-        let ageInMnt = ageInDays / 30;
+        let ageInMnt = ageInDays / 30,4375;
         let ageInYr = ageInMnt / 12;
 
         document.querySelector('#years').innerHTML = Math.floor(ageInYr);
         document.querySelector('#months').innerHTML = Math.floor(ageInMnt % 12);
-        document.querySelector('#days').innerHTML = Math.floor(ageInDays % 30);
+        document.querySelector('#days').innerHTML = Math.floor(ageInDays % 30),4375;
     }, 1000);
 }
 
